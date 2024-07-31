c9ca0991ff786bf09b56e318549a26c0638f9f48
chenlongxing <chenlongxing@2021>
2024-07-31 17:14:37 +0800

    used jQuery

--- popup.js
+++ popup.js
@@ -1,17 +1,17 @@
-
-let getImgBtn = document.getElementById('getImg');
-let tips = document.getElementById('tips');
-let videoUrl;
+let getImgBtn = $('#getImg');
+let tips = $('#tips');
+let videoUrl = "";
 let status = 0;
 let zipName = "文件";
 let imgList = [];
 let percent = 0;
-getImgBtn.onclick = function (e) {
+
+getImgBtn.on('click', function (e) {
     getData('获取资源', (response) => {
         console.log('资源内容', response);
         if (response && (response.zhu.length > 0 || response.xiang.length > 0)) {
-            getImgBtn.innerHTML = '已获取数据 √';
-            document.getElementById('download').style.cursor = 'pointer';
+            getImgBtn.html('已获取数据 √');
+            $('#download').css('cursor', 'pointer');
             if (response.name !== "") {
                 zipName = response.name.replaceAll(" ", "").replaceAll("\n", "").replaceAll("/", "_")
             }
@@ -37,47 +37,40 @@ getImgBtn.onclick = function (e) {
                 <div><img src="${sku[i].src}"/><span>${sku[i].name}</span></div>
                 `
             }
-            document.getElementById("imgList").innerHTML = _html;
-            tips.innerHTML = `当前选中 0/${imgList.length}`;
+            $("#imgList").html(_html);
+            tips.html(`当前选中 0/${imgList.length}`);
             // 添加选中事件
-            var items = document.getElementById('imgList').getElementsByTagName('div');
-            for (let i = 0; i < items.length; i++) {
-                items[i].onclick = function () {
-                    if (this.classList.contains('activation')) {
-                        //表示含有'checked'这个类名
-                        this.classList.remove('activation')
-                        let currentSelect = document.getElementById('imgList').querySelectorAll(".activation")
-                        tips.innerHTML = `当前选中 ${currentSelect.length}/${imgList.length}`
-                    } else {
-                        this.classList.add('activation')
-                        let currentSelect = document.getElementById('imgList').querySelectorAll(".activation")
-                        tips.innerHTML = `当前选中 ${currentSelect.length}/${imgList.length}`
-                    }
-                }
-            }
+            $('#imgList').find('div').on('click', function () {
+                // 如果指定的类存在，则移除；如果不存在，则添加
+                $(this).toggleClass('activation');
+                let currentSelect = $('#imgList').find(".activation").length;
+                tips.html(`当前选中 ${currentSelect}/${imgList.length}`);
+            });
             status = 0;
         } else {
             status = 0;
-            tips.innerHTML = '暂不支持当前页 ×';
+            tips.html('暂不支持当前页 ×');
         }
     });
-}
+});
 
-let downloadBtn = document.getElementById('download');
-downloadBtn.onclick = function () {
+let downloadBtn = $('#download');
+downloadBtn.on('click', function () {
     if (status === 0) {
         status = 1;
-        var list = document.getElementById("imgList").getElementsByClassName("activation") || [];
+        var list = $("#imgList").find(".activation") || [];
         var imgUrls = []
-        for (let i = 0; i < list.length; i++) {
-            imgUrls.push({ src: list[i].getElementsByTagName('img')[0].src, name: list[i].getElementsByTagName('span')[0].innerText })
-        }
+        list.each(function () {
+            let img = $(this).find('img');
+            let span = $(this).find('span');
+            imgUrls.push({ src: img.attr('src'), name: span.text() });
+        });
         if (imgUrls.length === 0) {
             status = 0;
-            tips.innerHTML = '未选择图片';
+            tips.html('未选择图片');
             return;
         }
-        tips.innerHTML = '正在打包中...';
+        tips.html('正在打包中...');
         var zip = new JSZip();
         try {
             saveToZip(zip, imgUrls, 0)
@@ -86,33 +79,42 @@ downloadBtn.onclick = function () {
             console.error(e);
         }
     } else {
-        tips.innerHTML = '打包中，请勿操作';
+        tips.html('打包中，请勿操作');
     }
-};
+});
+
 // 全选
-let allSelect = document.getElementById('allSelect')
-allSelect.onclick = function () {
-    let items = Array.from(document.getElementById('imgList').getElementsByTagName('div'));
-    let flag = items.filter(item => item.classList.contains('activation'));
+let allSelect = $('#allSelect');
+allSelect.on('click', function () {
+    let items = $('#imgList').find('div');
+    let flag = items.filter('.activation');
     if (items.length === 0) {
-        tips.innerHTML = '尚未获取图片';
+        tips.html('尚未获取图片');
         return;
     }
-    for (let i = 0; i < items.length; i++) {
+    // for (let i = 0; i < items.length; i++) {
+    //     if (flag.length < items.length) {
+    //         items[i].classList.add('activation');
+    //     } else {
+    //         items[i].classList.remove('activation');
+    //     }
+    // }
+    items.each(function () {
         if (flag.length < items.length) {
-            items[i].classList.add('activation');
+            $(this).addClass('activation');
         } else {
-            items[i].classList.remove('activation');
+            $(this).removeClass('activation');
         }
-    }
-    flag = items.filter(item => item.classList.contains('activation'));
-    tips.innerHTML = `当前选中 ${flag.length}/${imgList.length}`;
+    });
+    flag = items.filter('.activation');
+    tips.html(`当前选中 ${flag.length}/${imgList.length}`);
     if (flag.length === 0) {
-        allSelect.innerHTML = '全选图片';
+        allSelect.html('全选图片');
     } else {
-        allSelect.innerHTML = '取消全选图片';
+        allSelect.html('取消全选图片');
     }
-};
+});
+
 function getResources(index, url, callback) {
     let xhr = new XMLHttpRequest();
     xhr.open("get", url, true);
@@ -135,12 +137,13 @@ function getResources(index, url, callback) {
 
 function setPercent(index, length) {
     if (index === (length.length - 1)) {
-        document.getElementById("progress").value = 100;
+        $("#progress").val(100);
     } else {
         percent = parseInt(index / length * 100);
-        document.getElementById("progress").value = percent;
+        $("#progress").val(percent);
     }
 }
+
 function saveToZip(zip, imgUrls, index) {
     getResources(index + 1, imgUrls[index].src, (data, success) => {
         if (success) {
@@ -155,7 +158,7 @@ function saveToZip(zip, imgUrls, index) {
         setPercent(index, imgUrls.length)
         if (index >= imgUrls.length) {
             // 是否需要下载视频
-            if (videoUrl && document.getElementsByName("videoSelect")[0].checked) {
+            if (videoUrl && $("input[name='videoSelect']").prop('checked')) {
                 getResources(index + 1, videoUrl, (data, success) => {
                     if (success) {
                         zip.file(zipName + ".mp4", data);
@@ -163,14 +166,14 @@ function saveToZip(zip, imgUrls, index) {
                     zip.generateAsync({ type: "blob" }).then(function (content) {
                         saveAs(content, zipName + ".zip");
                         status = 0;
-                        tips.innerHTML = '打包完成';
+                        tips.html('打包完成');
                     });
                 });
             } else {
                 zip.generateAsync({ type: "blob" }).then(function (content) {
                     saveAs(content, zipName + ".zip");
                     status = 0;
-                    tips.innerHTML = '打包完成';
+                    tips.html('打包完成');
                 });
             }
         } else {
@@ -192,4 +195,3 @@ function getCurrentTabId(callback) {
         if (callback) callback(tabs.length ? tabs[0].id : null);
     });
 }
-
